8cfce2b8a957d4789e9a1eee5d368a50f25ff98e
Ed Mechem & Michael Furlong <pair+edmechem.mfurlong64@devbootcamp.com>
2016-07-19 12:11:57 -0700

    Complete request portion of all four routes

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   getTeachers();
   getTeacher();
   createBadge();
+  vote();
 });
 
 function getTeachers() {
@@ -37,9 +38,35 @@ function getTeacher() {
 function createBadge() {
   $('.add-badge').on('click', 'input[type="image"]', function(event) {
     event.preventDefault();
-    var newBadgeName = $(this).closest('form').serialize();
-    console.log(newBadgeName);
-    debugger;
-  })
+    var newBadgeData = $(this).closest('form').serialize();
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:3000/badges",
+      data: newBadgeData,
+      dataType: "json"
+    }).done(function(jsonifiedBadge){
+      // populate a 'partial' (handlebars?) with this json
+      // then display it
+    });
+  });
+};
 
-}
+function vote() {
+  $('.slogan').on('click', 'button', function(event) {
+    event.preventDefault();
+    var badgeID = $(this).closest('div').parent().attr('id');
+    var voteType = $(this).attr('class');
+    var voteData = 'badge_id=' + badgeID + '&vote_type=' + voteType;
+    console.log(voteData);
+    // debugger;
+    $.ajax({
+      method: "POST",
+      url: "http://localhost:3000/badges",
+      data: voteData,
+      dataType: "json"
+    }).done(function(jsonifiedBadge){
+      // populate a 'partial' (handlebars?) with this json
+      // then display it
+    });
+  });
+};
