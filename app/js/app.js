00954b587492f8368bd2be401136320a62f24526
Mikey Moore & Abid Ramay <pair+MikeyMoore.aramay@devbootcamp.com>
2016-07-19 17:17:39 -0700

    ajax call

--- app/js/app.js
+++ app/js/app.js
@@ -1,19 +1,24 @@
 $(function(){
   $("a").on("click", function(event){
     $this = $(this)
-    var renderIt = null
+
+    var thisData = {"badge": {"teacher_id": "1", "phrase": "no way"}}
+
+     $.ajax({
+        url:"http://sample-badges-api.herokuapp.com/badges",
+        method: "post",
+        data: thisData
+      }).done(function(response){
+        console.log(response)
+      }).fail(function(response){
+        console.log("fail")
+      })
+
     window.onhashchange = function(){
       $this
       // var teacherName = $this.text
       location.hash = $this[0].text
       // document.body.innerHTML = '<object type="text/html" data="sample-page.html" ></object>'
-
-      var ajaxRequest = $.ajax({
-        url :"http://sample-badges-api.herokuapp.com/",
-        method: "get",
-
-
-      })
     }
   });
 })
