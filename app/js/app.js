8dc66a6d52071325d7c5ab57ddc7eb165bd3100a
Mikey Moore & Abid Ramay <pair+MikeyMoore.aramay@devbootcamp.com>
2016-07-19 17:42:06 -0700

    Get route

--- app/js/app.js
+++ app/js/app.js
@@ -4,21 +4,35 @@ $(function(){
 
     var thisData = {"badge": {"teacher_id": "1", "phrase": "no way"}}
 
-     $.ajax({
-        url:"http://sample-badges-api.herokuapp.com/badges",
-        method: "post",
-        data: thisData
+     // $.ajax({
+     //    url:"http://sample-badges-api.herokuapp.com/badges",
+     //    method: "post",
+     //    data: thisData
+     //  }).done(function(response){
+     //    console.log(response);
+     //    $this.html(response.phrase)
+     //  }).fail(function(response){
+     //    console.log("fail")
+     //  })
+
+      $.ajax({
+        url:"http://sample-badges-api.herokuapp.com/teachers",
+        method: "get",
+        dataType: "json"
       }).done(function(response){
-        console.log(response)
+        var teachers = response
+        console.log(teachers)
+        for(i=0;i<teachers.length;i++){
+          console.log(teachers[i].name)
+        }
       }).fail(function(response){
         console.log("fail")
       })
 
+
     window.onhashchange = function(){
       $this
-      // var teacherName = $this.text
       location.hash = $this[0].text
-      // document.body.innerHTML = '<object type="text/html" data="sample-page.html" ></object>'
     }
   });
 })
